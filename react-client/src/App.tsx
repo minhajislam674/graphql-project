84ac75765de6fed2273bbe02ad9b92e78b3e16fb
Minhaj Islam <minhajislam674@gmail.com>
2023-04-25 03:33:18 +0200

    first commit

---
+++ react-client/src/App.tsx
@@ -0,0 +1,30 @@
+import { User } from "./types";
+import UserDisplay from "./components/UserDisplay";
+
+function App() {
+  const users: User[] = [
+    {
+      name: "Prisma Fan",
+      messages: [
+        {
+          body: "Prisma rocks!!",
+        },
+        {
+          body: "Did I mention I love Prisma?",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <>
+      <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
+        {users.map((user, index) => (
+          <UserDisplay user={user} key={index} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
