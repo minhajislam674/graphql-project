ab2d0726ade36f784a317d025c61adf2ff743d70
Minhaj Islam <minhajislam674@gmail.com>
2023-04-28 21:29:58 +0200

    Added the comment model

--- graphql-server/src/schema.ts
+++ graphql-server/src/schema.ts
@@ -1,23 +1,32 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { GraphQLContext } from "./context";
-import { Link } from "@prisma/client";
+import { Link, Comment } from "@prisma/client";
 
 //the type definitions (schema) are the structure of the GraphQL API, and they are defined using the GraphQL Schema Definition Language (SDL)
 const typeDefinitions = `
   type Query {
     info: String!
     feed: [Link!]!
+    comment(id: ID!): Comment
+    link(id: ID): Link
   }
 
   type Mutation {
     postLink (url: String!, description: String!): Link! 
+    postCommentOnLink (linkId: ID!, body: String!): Comment!
   }
  
-
   type Link {
     id: ID!
     description: String!
     url: String!
+    comments: [Comment!]!    
+  }
+
+  type Comment {
+    id: ID!
+    body: String!
+    link: Link
   }
 `;
 //the resolver functions are part of the GraphQL schema, and they are the actual implementation (code/logic) of the GraphQL schema
@@ -26,15 +35,60 @@ const resolvers = {
     info: () => `This is the API of a Hackernews Clone`,
     feed: (parent: unknown, args: {}, context: GraphQLContext) =>
       context.prisma.link.findMany(), //Prisma Client API exposes a number of database queries that let us read and write data in the database.
+    comment: (parent: unknown, args: { id: string }, context: GraphQLContext) =>
+      context.prisma.comment.findUnique({
+        where: {
+          id: parseInt(args.id),
+        },
+      }),
+    link: (parent: unknown, args: { id: string }, context: GraphQLContext) =>
+      context.prisma.link.findUnique({
+        where: {
+          id: parseInt(args.id),
+        },
+      }),
   },
 
   Link: {
     id: (parent: Link) => parent.id,
     description: (parent: Link) => parent.description,
     url: (parent: Link) => parent.url,
+    comments: (parent: Link, args: {}, context: GraphQLContext) => {
+      return context.prisma.comment.findMany({
+        where: {
+          linkId: parent.id,
+        },
+      });
+    },
+  },
+
+  Comment: {
+    id: (parent: Comment) => parent.id,
+    body: (parent: Comment) => parent.body,
+    link: (parent: Comment, args: {}, context: GraphQLContext) => {
+      return context.prisma.link.findUnique({
+        where: {
+          id: parent.linkId!,
+        },
+      });
+    },
   },
 
   Mutation: {
+    postCommentOnLink: (
+      parent: unknown,
+      args: { linkId: string; body: string },
+      context: GraphQLContext
+    ) => {
+      const newComment = context.prisma.comment.create({
+        data: {
+          body: args.body,
+          linkId: parseInt(args.linkId),
+        },
+      });
+      return newComment;
+    },
+
     postLink: (
       parent: unknown,
       args: { description: string; url: string },
